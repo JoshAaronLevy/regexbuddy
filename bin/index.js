6d42e86dd1b5ca68f1128c36a2e41642fbafb580
Josh Levy <joshaaronlevy@gmail.com>
2022-09-07 17:46:14 -0600

    updated version

--- bin/index.js
+++ bin/index.js
@@ -1,12 +1,13 @@
-export const filterValues = (...input) => {
+export const filterValues = async (...input) => {
 	if (input.length < 2) input.push(/[a-cA-c]+/);
 	console.log("input:", input);
 	const myRe = new RegExp(input[1]);
 	if (typeof input[0] !== 'string') input = input[0];
-	return input.filter(item => {
+	const regexBuddyResult = await input.filter(item => {
 		if (myRe.test(item)) {
 			console.log("item:", item);
 			return item;
 		};
 	});
+	return regexBuddyResult;
 }
