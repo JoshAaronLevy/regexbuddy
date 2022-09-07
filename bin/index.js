ae90ae76edffa9a310bb529b5f978b349189d1fc
Josh Levy <joshaaronlevy@gmail.com>
2022-09-07 17:36:59 -0600

    Updated filter expression

--- bin/index.js
+++ bin/index.js
@@ -1,6 +1,6 @@
 export const filterValues = (...input) => {
-	// In case an expression is not passed in, set a default one for the time being
 	if (input.length < 2) input.push(/[a-cA-c]+/);
+	console.log("input:", input);
 	const myRe = new RegExp(input[1]);
 	if (typeof input[0] !== 'string') input = input[0];
 	const regexBuddyResult = input.filter(item => {
