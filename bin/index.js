32ab1a559a6b6cdd602c59cc23c76dd13a09c63b
Josh Levy <joshaaronlevy@gmail.com>
2022-09-07 17:32:18 -0600

    Updated conditional for expression

--- bin/index.js
+++ bin/index.js
@@ -1,6 +1,7 @@
 export const filterValues = (...input) => {
-	const myRe = new RegExp(input[1]);
+	// In case an expression is not passed in, set a default one for the time being
 	if (input.length < 2) input.push(/[a-cA-c]+/);
+	const myRe = new RegExp(input[1]);
 	if (typeof input[0] !== 'string') input = input[0];
 	const regexBuddyResult = input.filter(item => {
 		if (myRe.test(item)) return item;
