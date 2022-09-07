863f4488d331340b9e84f144e9bac259c6ff3f50
Josh Levy <joshaaronlevy@gmail.com>
2022-09-07 17:54:18 -0600

    updated version

--- bin/index.js
+++ bin/index.js
@@ -1,13 +1,10 @@
-export const filterValues = async (...input) => {
+// ...input accepts the input from the user at index 0, and the desired expression at index 1
+export const filterValues = (...input) => {
+	// Next line is a default expression to test the function and condition for missing expression in the arguments passed in
 	if (input.length < 2) input.push(/[a-cA-c]+/);
-	console.log("input:", input);
 	const myRe = new RegExp(input[1]);
 	if (typeof input[0] !== 'string') input = input[0];
-	const regexBuddyResult = await input.filter(item => {
-		if (myRe.test(item)) {
-			console.log("item:", item);
-			return item;
-		};
+	return input.filter(item => {
+		if (myRe.test(item)) return item;
 	});
-	return regexBuddyResult;
 }
