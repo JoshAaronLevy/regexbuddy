586a9e7ab3ce0ba04c7c05c22db0f9f26fc5d9a1
Josh Levy <joshaaronlevy@gmail.com>
2022-09-07 17:27:36 -0600

    Added regex expression

--- bin/index.js
+++ bin/index.js
@@ -1,5 +1,10 @@
-
-export const replaceString = (input) => {
-	console.log("input:", input);
-	return input;
+const replaceString = (...input) => {
+	const myRe = new RegExp(input[1]);
+	if (input.length < 2) input.push(/[a-cA-c]+/);
+	if (typeof input[0] !== 'string') input = input[0];
+	const regexBuddyResult = input.filter(item => {
+		if (myRe.test(item)) return item;
+	});
+	console.log("regexBuddyResult:", regexBuddyResult);
+	return regexBuddyResult;
 }
