26a943a455ec0bb38fb317db273fcd2a5e4aabde
Josh Levy <joshaaronlevy@gmail.com>
2022-09-08 16:54:35 -0600

    updated version

--- bin/index.mjs
+++ bin/index.mjs
@@ -6,17 +6,18 @@ const validateEmail = (emailAddress) => {
 			console.error(`No email address provided`);
 		}
 		return false;
-	}
-	if (emailAddress.constructor.name !== 'String') {
-		console.error(`Invalid input type for email. Expected a string, but got ${emailAddress.constructor.name}`);
-		return false;
 	} else {
-		const myRe = new RegExp(/^[^@ ]+@[^@ ]+\.[^@ \.]{2,}$/);
-		if (!myRe.exec(emailAddress)) {
-			console.error(`Invalid email address: ${emailAddress}`);
+		if (emailAddress.constructor.name !== 'String') {
+			console.error(`Invalid input type for email. Expected a string, but got ${emailAddress.constructor.name}`);
 			return false;
 		} else {
-			return true;
+			const myRe = new RegExp(/^[^@ ]+@[^@ ]+\.[^@ \.]{2,}$/);
+			if (!myRe.exec(emailAddress)) {
+				console.error(`Invalid email address: ${emailAddress}`);
+				return false;
+			} else {
+				return true;
+			}
 		}
 	}
 }
